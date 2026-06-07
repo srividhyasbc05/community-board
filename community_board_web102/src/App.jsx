4d6f13b1c32bc1b74eafb45f4ace49b1bb668538
srividhyasbc05 <srividhya.sbc@gmail.com>
2026-06-07 15:57:08 -0700

    Initial project setup

---
+++ community_board_web102/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+const App = (props) => {
+
+  return (
+    <div className="App">
+      <h1>South Asian Spots in {props.city} </h1>
+
+      
+    </div>
+  )
+}
+
+export default App
